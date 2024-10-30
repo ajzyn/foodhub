3b69e52cd05e9bf5c7c6a0859d70cce548272850
ajzyn <props92@gmail.com>
2024-10-30 23:19:17 +0100

    supplier about page

--- app/(domains)/supplier/page.tsx
+++ app/(domains)/supplier/page.tsx
@@ -1,10 +1,16 @@
 'use client'
 
 import { useSession } from 'next-auth/react'
+import Link from 'next/link'
 
 export default function SupplierPage() {
   const { data: session } = useSession()
   console.log(session)
 
-  return <div>SupplierPage</div>
+  return (
+    <>
+      <div>SupplierPage</div>
+      <Link href="/about">About</Link>
+    </>
+  )
 }
