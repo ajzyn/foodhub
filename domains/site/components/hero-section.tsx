7254ca2258c026c65bac78b73e373cee6795828e
ajzyn <props92@gmail.com>
2024-10-30 21:09:04 +0100

    package.json updated

--- domains/site/components/hero-section.tsx
+++ domains/site/components/hero-section.tsx
@@ -3,6 +3,7 @@
 import { motion } from 'framer-motion'
 import { Button } from '@/components/ui/button'
 import { Car, Utensils } from 'lucide-react'
+import Link from 'next/link'
 
 export default function HeroSection() {
   return (
@@ -17,14 +18,22 @@ export default function HeroSection() {
           <h1 className="text-4xl font-bold mb-4">Simplify Ordering and Supply Management</h1>
           <p className="text-xl text-muted-foreground mb-8">Connect restaurants with the best suppliers effortlessly</p>
           <div className="flex flex-col md:flex-row justify-center md:space-x-4 space-y-4 md:space-y-0 mt-10">
-            <Button size="lg" variant="outline">
-              Zarejestruj się jako Restaurator
-              <Utensils className="ml-1" />
-            </Button>
-            <Button size="lg" variant="default">
-              Zarejestruj się jako Dostawca
-              <Car className="ml-1" />
-            </Button>
+            <Link
+              href={`${process.env.NEXT_PUBLIC_SCHEME}restaurant.${process.env.NEXT_PUBLIC_DOMAIN}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button size="lg" variant="outline">
+                Zarejestruj się jako Restaurator
+                <Utensils className="ml-1" />
+              </Button>
+            </Link>
+            <Link href={`supplier.${process.env.NEXT_PUBLIC_APP_URL}`}>
+              <Button size="lg" variant="default">
+                Zarejestruj się jako Dostawca
+                <Car className="ml-1" />
+              </Button>
+            </Link>
           </div>
         </motion.div>
       </div>
