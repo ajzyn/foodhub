e30ac496548bfbdebd5c0de455eff6c8e6342873
ajzyn <props92@gmail.com>
2024-11-02 21:20:30 +0100

    avoid infinite middleware run

--- middleware.ts
+++ middleware.ts
@@ -33,6 +33,6 @@ export default auth((request) => {
 
 export const config = {
   matcher: [
-    '/((?!_next|api|trpc|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)'
+    '/((?!_next|api|trpc|supplier|restaurant|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)'
   ]
 }
