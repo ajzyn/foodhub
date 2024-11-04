7469338a067149a3ddeed0b585a94adb63b34152
ajzyn <props92@gmail.com>
2024-11-04 19:43:54 +0100

    test

--- auth.ts
+++ auth.ts
@@ -12,10 +12,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       name: `session-token`,
       options: {
         httpOnly: true,
-        sameSite: 'lax',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         path: '/',
-        secure: process.env.NEXTAUTH_URL?.includes('https')
-        // domain: process.env.NODE_ENV === 'production' ? `.${process.env.NEXT_PUBLIC_DOMAIN}` : '.myapp.local'
+        secure: process.env.NEXTAUTH_URL?.includes('https'),
+        domain: process.env.NODE_ENV === 'production' ? `.${process.env.NEXT_PUBLIC_DOMAIN}` : '.myapp.local'
       }
     }
   },
