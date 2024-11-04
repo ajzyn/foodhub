253f8d8340d92b7cf9e8f84f532cacf574db82d3
ajzyn <props92@gmail.com>
2024-11-04 20:02:24 +0100

    test

--- middleware.ts
+++ middleware.ts
@@ -17,31 +17,32 @@ export default auth((request) => {
 
   const pathWithSearchParams = url.pathname + (searchParams ? `?${searchParams}` : '')
 
-  console.log('przed custom subdomain')
+  // console.log('przed custom subdomain')
   if (customSubdomain !== hostname) {
-    console.log('custom subdomain')
+    // console.log('custom subdomain')
     if (pathWithSearchParams.startsWith(`/${customSubdomain}`)) {
-      console.log('w ifie')
+      // console.log('w ifie')
       const purePath = pathWithSearchParams.split(`/${customSubdomain}`)[1]
 
       url.pathname = `/${purePath}`
 
       return NextResponse.redirect(url)
     }
-    console.log('za ifem w custom subdomain')
+    // console.log('za ifem w custom subdomain')
 
     url.pathname = `/${customSubdomain}${pathWithSearchParams}`
     console.log(url)
+    // console.log(url)
     return NextResponse.rewrite(url)
   }
 
   if (url.pathname === '/') {
-    console.log('w /')
+    // console.log('w /')
     url.pathname = '/site'
     return NextResponse.rewrite(url)
   }
 
-  console.log('next()')
+  // console.log('next()')
   return NextResponse.next()
 })
 
