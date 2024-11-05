2c9748f60d4f13b0235a52c84ac2b6470c6c361c
ajzyn <props92@gmail.com>
2024-11-05 23:11:38 +0100

    test

--- auth.ts
+++ auth.ts
@@ -12,7 +12,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       name: `session-token`,
       options: {
         httpOnly: true,
-        sameSite: 'lax',
+        sameSite: 'none',
         path: '/',
         secure: process.env.NEXTAUTH_URL?.includes('https'),
         domain: process.env.NODE_ENV === 'production' ? `.${process.env.NEXT_PUBLIC_DOMAIN}` : '.myapp.local'
