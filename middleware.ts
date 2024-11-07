065ae2058de7614633ee3b163a5513c3cc0928d2
ajzyn <props92@gmail.com>
2024-11-07 08:20:22 +0100

    middleware test

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,10 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-import { auth } from './auth'
-// export default async function middleware(request: NextRequest) {
+import { getSession } from 'next-auth/react'
+export default async function middleware(request: NextRequest) {
+  const session = await getSession()
+  console.log(session)
 
-// }
-
-export default auth((request) => {
-  console.log(request.cookies.get('session-token')?.value)
   const hostname = request.headers.get('host')
   const domain = process.env.NEXT_PUBLIC_DOMAIN
 
@@ -47,7 +45,53 @@ export default auth((request) => {
   // Continue with the original request if no conditions are met
   console.log('No rewrite or redirect needed, continuing with the original request')
   return NextResponse.next()
-})
+}
+
+// export default auth(async (request) => {
+//   const session = await getSession()
+//   console.log(session)
+
+//   const hostname = request.headers.get('host')
+//   const domain = process.env.NEXT_PUBLIC_DOMAIN
+
+//   // Clone the URL to manipulate the path
+//   const url = request.nextUrl.clone()
+
+//   console.log('Incoming request for:', hostname, 'Path:', url.pathname)
+
+//   // Check if the domain variable is set correctly
+//   if (!domain) {
+//     console.error('NEXT_PUBLIC_DOMAIN is not defined')
+//     return NextResponse.next()
+//   }
+
+//   // Handle auth paths separately
+//   if (url.pathname.startsWith('/auth/')) {
+//     console.log('Auth path detected, bypassing rewrite:', url.pathname)
+//     return NextResponse.next()
+//   }
+
+//   // Redirect main domain to /site
+//   if (hostname === domain) {
+//     console.log('Redirecting main domain to /site')
+//     url.pathname = '/site'
+//     return NextResponse.rewrite(url)
+//   }
+
+//   // Rewrite subdomain to specific path
+//   const subdomain = hostname?.split(`.${domain}`)[0]?.toLowerCase()
+//   console.log('Subdomain detected:', subdomain)
+
+//   if (subdomain === 'supplier') {
+//     console.log('Rewriting supplier subdomain to /supplier')
+//     url.pathname = '/supplier'
+//     return NextResponse.rewrite(url)
+//   }
+
+//   // Continue with the original request if no conditions are met
+//   console.log('No rewrite or redirect needed, continuing with the original request')
+//   return NextResponse.next()
+// })
 //   const hostname = request.headers.get('host')
 //   const domain = process.env.NEXT_PUBLIC_DOMAIN
 
