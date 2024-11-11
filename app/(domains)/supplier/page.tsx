8c7364caefac360e9f17a4ad4b278b02dc612d94
ajzyn <props92@gmail.com>
2024-11-11 23:02:31 +0100

    react query config

--- app/(domains)/supplier/page.tsx
+++ app/(domains)/supplier/page.tsx
@@ -1,12 +1,8 @@
 'use client'
 
-import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 
 export default function SupplierPage() {
-  const { data: session } = useSession()
-  console.log(session)
-
   return (
     <>
       <div>SupplierPage</div>
--- components/providers/combined-providers.tsx
+++ components/providers/combined-providers.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { SessionProvider } from '@/components/providers/session-provider'
+import ReactQueryProvider from './react-query-provider'
 
 export default function CombinedProviders({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      <SessionProvider>{children}</SessionProvider>
+      <ReactQueryProvider>
+        <SessionProvider>{children}</SessionProvider>
+      </ReactQueryProvider>
     </ThemeProvider>
   )
 }
