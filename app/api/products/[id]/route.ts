5308f5dc4057a9c842ce667eefae606c09f49775
ajzyn <props92@gmail.com>
2024-11-14 14:44:15 +0100

    test

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -6,14 +6,10 @@ import getSession from '@/lib/get-session'
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
   const session = await getSession()
-  return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
 
-  console.log(session)
   if (!session) {
     return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
   }
-
-  console.log(params.id)
 
   try {
     const product = await getProductById(params.id)
